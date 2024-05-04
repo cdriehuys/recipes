d9058eadbdf832f56c561ae5a8c9a7c0dbb8b4a4
Chathan Driehuys <chathan@driehuys.com>
2024-05-04 22:18:10 +0000

    Database integration

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log/slog"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/cdriehuys/recipes/internal/staticfiles"
 	"github.com/cdriehuys/recipes/internal/templates"
@@ -25,12 +29,46 @@ func index(logger *slog.Logger, templates TemplateEngine) http.HandlerFunc {
 	}
 }
 
+func dbTest(logger *slog.Logger, pool *pgxpool.Pool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var message string
+		if err := pool.QueryRow(r.Context(), "SELECT 'Hello, database!'").Scan(&message); err != nil {
+			logger.Error("Failed to get database message.", "error", err)
+			return
+		}
+
+		io.WriteString(w, message)
+	}
+}
+
+func dbConnectionURL() url.URL {
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	host := os.Getenv("POSTGRES_HOSTNAME")
+	db := os.Getenv("POSTGRES_DB")
+
+	return url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword(user, password),
+		Host:   host,
+		Path:   db,
+	}
+}
+
 func main() {
 	logOpts := slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &logOpts))
-	logger.Info("Creating request handlers.")
+
+	connURL := dbConnectionURL()
+	dbpool, err := pgxpool.New(context.Background(), connURL.String())
+	if err != nil {
+		logger.Error("Unable to create database connection pool.", "error", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+	logger.Info("Created database connection pool.")
 
 	templateEngine := templates.DiskTemplateEngine{
 		IncludePath: "templates/includes",
@@ -40,8 +78,10 @@ func main() {
 
 	staticServer := staticfiles.StaticFilesFromDisk{BasePath: "static"}
 
+	logger.Info("Creating request handlers.")
 	handler := http.NewServeMux()
 	handler.HandleFunc("/", index(logger, &templateEngine))
+	handler.HandleFunc("/db", dbTest(logger, dbpool))
 	handler.Handle("/static/", http.StripPrefix("/static/", &staticServer))
 
 	server := http.Server{
