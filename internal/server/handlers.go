9c73b01524e9dfa260eba28620bc16cd05e02610
Chathan Driehuys <chathan@driehuys.com>
2024-05-05 01:33:13 +0000

    Inserting new recipes

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,11 +1,14 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+)
 
 func indexHandler(state *State) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		logger := state.Logger.WithGroup("request").With("method", r.Method, "path", r.URL.Path)
-		logger.Info("Handling request.")
+		logger := state.requestLogger(r)
 
 		if err := state.TemplateEngine.Write(w, "index", nil); err != nil {
 			logger.Error("Failed to execute template.", "error", err)
@@ -13,10 +16,43 @@ func indexHandler(state *State) http.HandlerFunc {
 	}
 }
 
+func addRecipeHandler(state *State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := state.requestLogger(r)
+
+		if err := state.TemplateEngine.Write(w, "add-recipe", nil); err != nil {
+			logger.Error("Failed to execute template.", "error", err)
+		}
+	}
+}
+
+func addRecipeFormHandler(state *State) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger := state.requestLogger(r)
+
+		id := uuid.New()
+		title := r.FormValue("title")
+		instructions := r.FormValue("instructions")
+
+		query := `INSERT INTO recipes (id, title, instructions) VALUES ($1, $2, $3)`
+		if _, err := state.Db.Exec(r.Context(), query, id, title, instructions); err != nil {
+			logger.Error("Failed to insert new recipe.", "error", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		logger.Info("Inserted new recipe.", "id", id)
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
 func MakeHandler(state State) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", indexHandler(&state))
+	mux.HandleFunc("GET /new-recipe", addRecipeHandler(&state))
+	mux.HandleFunc("POST /new-recipe", addRecipeFormHandler(&state))
 
 	return mux
 }
--- internal/server/state.go
+++ internal/server/state.go
@@ -3,6 +3,7 @@ package server
 import (
 	"io"
 	"log/slog"
+	"net/http"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -15,4 +16,11 @@ type State struct {
 	Db             *pgxpool.Pool
 	Logger         *slog.Logger
 	TemplateEngine TemplateEngine
+}
+
+func (s *State) requestLogger(req *http.Request) *slog.Logger {
+	logger := s.Logger.With(slog.Group("request", "method", req.Method, "path", req.URL.Path))
+	logger.Info("Handling request.")
+
+	return logger
 }
