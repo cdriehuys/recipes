fa0b2fd230529ddc80b1431a1c49bdaecea506a0
Chathan Driehuys <chathan@driehuys.com>
2024-05-24 22:13:50 +0000

    feat: Hamburger menu on smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["templates/**/*.html.tmpl"],
+  content: ["templates/**/*.html.tmpl", "static/**/*.js"],
   theme: {
     fontFamily: {
       "serif": 'Lora, ui-serif, Georgia, Cambria, "Times New Roman", Times, serif'
