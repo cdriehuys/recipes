e85280922d06962be1c0ad228af665eb2a97ff1b
Chathan Driehuys <chathan@driehuys.com>
2024-06-02 13:34:34 +0000

    refactor: Reduce queries for recording log in
    Our original method for recording a log in used two queries because it
    was easier to tell if we inserted a new record into the `users` table.
    We actually want to look at if the user's name is empty to determine if
    the user is "new". This allows us to use a single query.

--- internal/models/users.go
+++ internal/models/users.go
@@ -35,26 +35,22 @@ func (model *UserModel) Exists(ctx context.Context, id string) (bool, error) {
 // Record the log in for a user. Returns a boolean indicating if the user needs to complete their
 // registration as well as any error that occurred.
 func (model *UserModel) RecordLogIn(ctx context.Context, id string) (bool, error) {
-	// This process is done as 2 separate queries to make it easy to tell if we added a new user.
-	// With one query that upserted a user and set the new login time, it was difficult to tell if
-	// the user was already present in the database.
+	insert := `INSERT INTO "users" (id, name, last_login) VALUES ($1, '', now())
+		ON CONFLICT (id) DO UPDATE
+		SET last_login = now()
+		RETURNING name`
 
-	insert := `INSERT INTO "users" (id, name) VALUES ($1, '') ON CONFLICT DO NOTHING`
-	_, err := model.DB.Exec(ctx, insert, id)
-	if err != nil {
-		return false, fmt.Errorf("failed to persist user: %w", err)
-	}
-
-	model.Logger.Info("Authenticated user persisted.", "id", id)
-
-	loginUpdate := `UPDATE "users" SET last_login = now() WHERE id = $1 RETURNING name`
 	var name string
-	if err := model.DB.QueryRow(ctx, loginUpdate, id).Scan(&name); err != nil {
+	if err := model.DB.QueryRow(ctx, insert, id).Scan(&name); err != nil {
 		return false, fmt.Errorf("failed to update user login time: %w", err)
 	}
 
 	// If the user has no name, that's a signal they need to complete their registration.
-	return name == "", nil
+	created := name == ""
+
+	model.Logger.InfoContext(ctx, "Recorded user login.", "id", id, "newUser", created)
+
+	return created, nil
 }
 
 func (model *UserModel) UpdateName(ctx context.Context, id, name string) error {
